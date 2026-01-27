77497909f3d8f53654a1c40f5a65275e86502dbf
Mukul Bhatt <mukulgautam5200@gmail.com>
2026-01-27 09:36:33 +0530

    changed navigation to path based - testing remains

---
+++ DatingAppFrontend/Navigation/AuthFlowView.swift
@@ -0,0 +1,56 @@
+//
+//  AuthFlowView.swift
+//  DatingAppFrontend
+//
+//  Created by Mukul Bhatt on 25/01/26.
+//
+
+import SwiftUI
+
+enum Route: Hashable {
+    case login
+    case loginOtp
+    case register
+    case registerOtp
+    case imageSelectionView
+    case profileSetup
+    case interests
+}
+
+struct AuthFlowView: View {
+
+    @StateObject private var viewModel = ProfileViewModel()
+    @State private var path = NavigationPath()
+
+    var body: some View {
+
+        NavigationStack(path: $path) {
+            LandingScreenView(path: $path)
+                .navigationDestination(for: Route.self) { route in
+                switch route {
+                case .login:
+                    LoginView(viewModel: viewModel, path: $path)
+
+                case .loginOtp:
+                    LoginOtpVerificationView(viewModel: viewModel, path: $path)
+
+                case .register:
+                    RegisterWithUsView(viewModel: viewModel, path: $path)
+
+                case .registerOtp:
+                    RegisterOtpVerificationView(viewModel: viewModel, path: $path)
+
+                case .imageSelectionView:
+                    ImageSelectionView(viewModel: viewModel, path: $path)
+
+                case .profileSetup:
+                    ProfileSetup(viewModel: viewModel, path: $path)
+
+                case .interests:
+                    InterestsView(viewModel: viewModel, path: $path)
+                }
+            }
+        }
+        
+    }
+}
